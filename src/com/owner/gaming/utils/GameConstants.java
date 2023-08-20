259167de87db5ee25a92cc1a65c4bb6b1bb4a5c4
Memansha <memansha2403@gmail.com>
2023-08-21 00:52:59 +0530

    First commit

---
+++ src/com/owner/gaming/utils/GameConstants.java
@@ -0,0 +1,17 @@
+package com.owner.gaming.utils;
+
+public interface GameConstants {
+	int GHEIGHT = 700; // public static final int GHEIGHT = 700;
+	int GWIDTH = 1250;
+	
+	String TITLE = "Combat";
+	int FLOOR = GHEIGHT - 150;
+	String RYU_IMAGE = "player-sprite.gif";
+	String KEN_IMAGE = "kenimage.png";
+	String RYU_FLIP_IMAGE = "ryuFlipImage.png";
+	int SPEED = 20;
+	int DEFAULT_FORCE = -20;
+	int GRAVITY = 1;
+	int MAX_HEALTH = 300;
+
+}
